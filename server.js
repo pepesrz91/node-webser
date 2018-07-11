7d4944636cc79bb9a75d03b7ec384a489148198f
DESKTOP-2UG043F\pepe_ <pepesrzra@outlook.com>
2018-07-11 14:09:30 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set('view engine','hbs');
+
+
+app.use((req,res,next)=>{
+  var now = new Date().toString();
+  var log = `${now}:${req.method} ${req.url}`
+  console.log(log);
+  fs.appendFile('server.log',log + '\n',(err)=>{
+    if (Error) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+// app.use((req,res,next)=>{
+//   res.render('maintenance.hbs',{
+//     title:"We are evolving!",
+//     message:"Please comeback later!"
+//   });
+// });
+
+app.use(express.static(__dirname + "/public"));
+
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(text)=>{
+  return text.toUpperCase();
+});
+
+app.get('/',(req, res)=>{
+  // res.send('<h1>Hello Express!!</h1>');
+  res.render('home.hbs',{
+    name:'Pepe',
+    likes:[
+      'Golf',
+      'Games',
+      'Party',
+      'Muuuuuuuusic!'
+    ],
+    homepage:'Home',
+    pageTitle:'Welcome to Express!!',
+    //currentYear:new Date().getFullYear()
+  });
+});
+
+app.get('/about',(req,res)=>{
+  //res.send('<h1>About Page</h1>');
+  res.render('about.hbs',{
+    pageTitle:'About Page',
+    //currentYear:new Date().getFullYear()
+  });
+});
+
+app.get('/bad',(req,res)=>{
+  res.send({
+    errorMessage:'Unable to handle Request',
+  });
+});
+app.listen(3000,()=>{
+  console.log('Site is alive at port 3000');
+});
